97ab6be5b398e51d886c0c0fccf35bc197ce52d8
meierrene <renemeier.de@gmail.com>
2022-05-24 23:39:55 -0300

    make it better

--- projects/Where-Am-I/script.js
+++ projects/Where-Am-I/script.js
@@ -12,11 +12,12 @@ const getLon = document.querySelector('.input--longitude');
 
 getLat.focus();
 
-const renderCountry = function (data, className = '') {
+const renderCountry = function (data, className = '', i) {
   const html = `
   <article class="country ${className}">
-       <img class="country__img" src="${data.flags.svg}" />
-       <div class="country__data">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+  ${className ? `<p class="country__row">Neighbour ${i + 1}</p>` : ''}
        <h3 class="country__name">${data.name.common}</h3>
        <h4 class="country__region">${data.region}</h4>
        <p class="country__row"><span>🏙️</span>${data.capital[0]}</p>
@@ -115,14 +116,14 @@ const whereAmI = async function (lat, lon) {
     const neighbours = data[0].borders;
 
     if (neighbours) {
-      neighbours.forEach(async country => {
+      neighbours.forEach(async (country, i) => {
         const res = await fetch(
           `https://restcountries.com/v3.1/alpha/${country}`
         );
         if (!res.ok) throw new Error(`Problem getting neighbour country`);
 
         const data = await res.json();
-        renderCountry(data[0], 'neighbour');
+        renderCountry(data[0], 'neighbour', i);
       });
     }
 
