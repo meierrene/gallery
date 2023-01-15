4ea388a3a914bd5095d1bb585631f3a4b0c6a878
meierrene <renemeier.de@gmail.com>
2023-01-15 11:50:51 -0300

    Mario game added

--- projects/Bankist Homepage/script.js
+++ projects/Bankist Homepage/script.js
@@ -11,8 +11,9 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
-//-------------------------------------------------
+//---------------------------------------------------------------
 // Modal window
+//---------------------------------------------------------------
 
 const openModal = function (e) {
   e.preventDefault();
@@ -36,44 +37,17 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//-------------------------------------------------
+//---------------------------------------------------------------
 // Button scrolling
+//---------------------------------------------------------------
 
 btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-  // console.log(
-  //   'height/width viewport',
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth
-  // );
-
-  // Scrolling
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-//-------------------------------------------------
+//---------------------------------------------------------------
 // Page navigation
-
-// document.querySelectorAll('.nav__link').forEach(btn =>
-//   btn.addEventListener('click', function (e) {
-//     e.preventDefault();
-
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//   })
-// );
-
-// 1. Add event listener to common parent element
-// 2.Determine what element originated the event
+//---------------------------------------------------------------
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
@@ -85,8 +59,9 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-//-------------------------------------------------
+//---------------------------------------------------------------
 // Tabbed component
+//---------------------------------------------------------------
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -106,7 +81,10 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 
+//---------------------------------------------------------------
 // Menu fade animation
+//---------------------------------------------------------------
+
 const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
@@ -120,35 +98,8 @@ const handleHover = function (e) {
   }
 };
 
-// Passing "Argument" into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
-
 nav.addEventListener('mouseout', handleHover.bind(1));
-
-// // Sticky navigation
-// const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
-// window.addEventListener('scroll', function () {
-//   console.log(window.scrollY);
-
-//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// });
-
-// // Sticky navigation: Intersection Observer API
-// const obsCallback = function (entries, observer) {
-//   entries.forEach(entry => {
-//     console.log(entry);
-//   });
-// };
-
-// const obsOptions = {
-//   root: null,
-//   threshold: [0, 0.2],
-// };
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
-// observer.observe(section1);
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
@@ -167,7 +118,10 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+//---------------------------------------------------------------
 // Reveal sections
+//---------------------------------------------------------------
+
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
@@ -188,8 +142,10 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
-
+//---------------------------------------------------------------
 // Lazy loading images
+//---------------------------------------------------------------
+
 const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImg = function (entries, observer) {
@@ -215,7 +171,10 @@ const imgObserver = new IntersectionObserver(loadImg, {
 
 imgTargets.forEach(img => imgObserver.observe(img));
 
+//---------------------------------------------------------------
 // Slider
+//---------------------------------------------------------------
+
 const slider = function () {
   const slides = document.querySelectorAll('.slide');
   const btnLeft = document.querySelector('.slider__btn--left');
@@ -298,185 +257,3 @@ const slider = function () {
   });
 };
 slider();
-//-------------------------------------------------
-// Lectures
-//-------------------------------------------------
-
-// Selecting elements
-
-// console.log(document.documentElement); // Shows the assigned HTML
-// console.log(document.head); // Either to show only head from HTML as well
-// console.log(document.body);
-
-// const header = document.querySelector('.header');
-// const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
-
-// document.getElementById('section--1');
-// const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName('btn'));
-
-// Creating and inserting elements
-
-// .insertAdjacentHTML
-
-// const message = document.createElement('div');
-// message.classList.add('cookie-message');
-// message.textContent =
-//   'We use cookies for improved functionality and analytics.'; // Only text only will be shown. So the Tags will not longer work!
-// message.innerHTML =
-//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'; // This the best way to add texts, tags, and so on and for forth instead!
-
-// header.prepend(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
-// header.before(message);
-// header.after(message);
-
-// Delete elements
-
-// document
-//   .querySelector('.btn--close-cookie')
-//   .addEventListener('click', function () {
-// message.remove(); // new way!
-//   message.parentElement.removeChild(message);
-// });
-
-// Styles
-
-// message.style.backgroundColor = '#37383d';
-// message.style.width = '120%';
-
-// console.log(message.style.color);
-// console.log(message.style.backgroundColor);
-
-// console.log(getComputedStyle(message).color);
-// console.log(getComputedStyle(message).height);
-
-// message.style.height =
-//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
-
-// document.documentElement.style.setProperty('--color-primary', 'blue');
-
-// Attributes
-
-// const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt);
-// console.log(logo.src); // Only owrks for standard properties
-// console.log(logo.className);
-
-// logo.alt = 'Beautiful minimalist logo';
-
-// Non-standard attributes
-
-// console.log(logo.designer); // Will show in blank
-// console.log(logo.getAttribute('designer'));
-// logo.setAttribute('company', 'Bankist');
-
-// console.log(logo.src);
-// console.log(logo.getAttribute('src'));
-
-// const link = document.querySelector('.nav__link--btn');
-// console.log(link.href);
-// console.log(link.getAttribute('href'));
-
-// Data attributes
-
-// console.log(logo.dataset.versionNumber);
-
-// // Classes
-
-// logo.classList.add('c', 'j');
-// logo.classList.remove('c', 'j');
-// logo.classList.toggle('c');
-// logo.classList.contains('c');
-
-// // Don't use!
-// logo.className = 'name';
-
-// const h1 = document.querySelector('h1');
-
-// const alertH1 = function (e) {
-//   alert('addEventListerner works');
-// };
-
-// h1.addEventListener('mouseenter', alertH1);
-
-// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
-// h1.onmouseenter = function (e) {
-//   alert('addEventListerner works');
-// };
-
-// rgb(255,255,255)
-// const randomInt = (min, max) =>
-//   Math.floor(Math.random() * (max - min + 1) + min);
-// const randomColor = () =>
-//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-
-// document.querySelector('.nav__link').addEventListener('click', function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log('LINK', e.target, e.currentTarget);
-//   console.log(e.currentTarget === this);
-
-//   // Stop propagation
-//   // e.stopPropagation();
-// });
-
-// document.querySelector('.nav__links').addEventListener('click', function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log('CONTAINER', e.target, e.currentTarget);
-// });
-
-// document.querySelector('.nav').addEventListener(
-//   'click',
-//   function (e) {
-//     this.style.backgroundColor = randomColor();
-//     console.log('NAV', e.target, e.currentTarget);
-//   },
-//   true
-// );
-
-// const h1 = document.querySelector('h1');
-
-// console.log(h1.querySelectorAll('.highlight'));
-// console.log(h1.childNodes);
-// console.log(h1.children);
-// h1.firstElementChild.style.color = 'white';
-// h1.lastElementChild.style.color = 'orangered';
-
-// // Going upwards: parents
-// console.log(h1.parentNode);
-// console.log(h1.parentElement);
-
-// h1.closest('.header').style.background = 'var(--gradient-primary)';
-// h1.closest('h1').style.background = 'var(--gradient-secondary)';
-
-// // Going sideways: siblings
-// console.log(h1.previousElementSibling);
-// console.log(h1.nextElementSibling);
-
-// console.log(h1.previousSibling);
-// console.log(h1.nextSibling);
-
-// console.log(h1.parentElement.children);
-// [...h1.parentElement.children].forEach(function (el) {
-//   if (el !== h1) el.style.transform = 'scale(0.5)';
-// });
-
-// document,
-//   addEventListener('DOMContentLoaded', function (e) {
-//     console.log('HTML parsed and DOM tree built!', e);
-//   });
-
-// window.addEventListener('load', function (e) {
-//   console.log('Page fully loaded', e);
-// });
-
-// window.addEventListener('beforeunload', function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   e.returnValue = '';
-// });
