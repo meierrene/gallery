33a606c1b50cd2fb80507130140b3c674d7cdff4
René Meier <renemeier.de@gmail.com>
2024-09-10 20:00:08 -0300

    small bug fixes

--- projects/WhatsApp-Message/script.js
+++ projects/WhatsApp-Message/script.js
@@ -11,43 +11,29 @@ const getCode = async () => {
   try {
     const res = await fetch(APIADDRESS);
     const data = await res.json();
-
-    const code = countryCode.value;
-    if (code) {
-      const countryData = data.find(d => {
-        const idd =
-          d.idd && d.idd.root && d.idd.suffixes
-            ? d.idd.root + d.idd.suffixes
-            : '';
-        return idd === code;
-      });
-
-      if (countryData) {
-        renderCountry(countryData.cca3, countryData.flags.svg);
-      } else {
-        aCountry.innerHTML = '<p>Country not found</p>';
-        aCountry.classList.remove('hidden');
-      }
-    }
+    data.forEach(d => {
+      const idd = +(d.idd.root + d.idd.suffixes);
+      idd === +countryCode.value ? renderCountry(d.cca3, d.flags.svg) : '';
+    });
   } catch (err) {
-    console.error('Error fetching country data:', err);
+    console.error(err);
+    throw err;
   }
 };
 
 const renderCountry = (country, flag) => {
-  aCountry.innerHTML = `<div class="flex-icon"><p>${country}</p><img src="${flag}" alt="Flag of ${country}"></div>`;
+  aCountry.innerHTML = `<div class="flex-icon"><p>${country}</p><img src="${flag}"></div>`;
   aCountry.classList.remove('hidden');
 };
+
+countryCode.focus();
 
 countryCode.addEventListener('focusout', getCode);
 
 sendBtn.addEventListener('click', () => {
-  const phone = `${countryCode.value}${regionCode.value}${phoneNumber.value}`;
-  if (phone.length > 0) {
-    document.querySelector('.form').action = `https://wa.me/${phone}`;
-  } else {
-    alert('Please fill in all fields before sending.');
-  }
+  document.querySelector(
+    '.form'
+  ).action = `https://wa.me/${countryCode.value}${regionCode.value}${phoneNumber.value}`;
 });
 
-document.querySelector('.aYear').textContent = new Date().getFullYear();
+document.querySelector('.aYear').innerHTML = new Date().getFullYear();
